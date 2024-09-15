be8c90ef068da75030bb8b90602f77aad091af84
missmaileech <missmaileech@gmail.com>
2024-09-15 07:00:58 +0530

    Add files via upload

---
+++ Savory/__main__.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import glob
+import asyncio
+import logging
+import importlib
+from pathlib import Path
+from pyrogram import idle
+from .bot import StreamBot
+from .vars import Var
+from aiohttp import web
+from .server import web_server
+from .utils.keepalive import ping_server
+from Savory.bot.clients import initialize_clients
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+
+ppath = "Savory/bot/plugins/*.py"
+files = glob.glob(ppath)
+StreamBot.start()
+loop = asyncio.get_event_loop()
+
+
+# fix for current pyrogram 
+from pyrogram import utils
+
+def get_peer_type_new(peer_id: int) -> str:
+    peer_id_str = str(peer_id)
+    if not peer_id_str.startswith("-"):
+        return "user"
+    elif peer_id_str.startswith("-100"):
+        return "channel"
+    else:
+        return "chat"
+utils.get_peer_type = get_peer_type_new
+
+
+
+async def start_services():
+    print('\n')
+    print('------------------- Initalizing Telegram Bot -------------------')
+    bot_info = await StreamBot.get_me()
+    StreamBot.username = bot_info.username
+    print("------------------------------ DONE ------------------------------")
+    print()
+    print(
+        "---------------------- Initializing Clients ----------------------"
+    )
+    await initialize_clients()
+    print("------------------------------ DONE ------------------------------")
+    print('\n')
+    print('--------------------------- Importing ---------------------------')
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"Savory/bot/plugins/{plugin_name}.py")
+            import_path = ".plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["Savory.bot.plugins." + plugin_name] = load
+            print("Imported => " + plugin_name)
+    if Var.ON_HEROKU:
+        print("------------------ Starting Keep Alive Service ------------------")
+        print()
+        asyncio.create_task(ping_server())
+    print('-------------------- Initalizing Web Server -------------------------')
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADRESS
+    await web.TCPSite(app, bind_address, Var.PORT).start()
+    print('----------------------- Service Started -----------------------------------------------------------------')
+    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
+    print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
+    print('                        Owner =>> {}'.format((Var.OWNER_USERNAME)))
+    if Var.ON_HEROKU:
+        print('                        app runnng on =>> {}'.format(Var.FQDN))
+    await idle()
+
+if __name__ == '__main__':
+    try:
+        loop.run_until_complete(start_services())
+    except KeyboardInterrupt:
+        logging.info('----------------------- Service Stopped -----------------------')
